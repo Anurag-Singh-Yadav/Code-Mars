c80695e09fe534e7e963739f09bc6c0df9837365
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2023-11-04 02:16:23 +0530

    problem navbar

--- src/components/user/ProblemsNavBar.jsx
+++ src/components/user/ProblemsNavBar.jsx
@@ -7,14 +7,14 @@ function ProblemsNavBar({showTags , setShowTags}) {
     }
 
   return (
-    <div className='flex justify-center gap-4 pt-4 items-center'>
-        <p className='text-[#ff4545] font-[650]'>We recommend hiding tags before for questions!</p>
+    <div className='flex flex-col sm:flex-row justify-center gap-2 sm:gap-4 pt-2 sm:pt-4 items-center'>
+        <p className='text-[#ff4545] font-medium text-sm sm:text-lg md:text-xl md:font-[650]'>We recommend hiding tags before for questions!</p>
 
         {
             showTags ? (
-                <p onClick={toggleTags} className='bg-[#3b71fc] text-white font-[600] px-3 py-1 rounded-lg hover:bg-[#445b96] hover:cursor-pointer'>Hide tags</p>
+                <p onClick={toggleTags} className='bg-[#3b71fc] text-white text-sm px-1 sm:px-2 md:font-[600] md:px-3 py-1 rounded-lg hover:bg-[#445b96] hover:cursor-pointer'>Hide tags</p>
             ) : (
-                <p onClick={toggleTags} className='bg-[#3b71fc] text-white font-[600] px-3 py-1 rounded-lg hover:bg-[#445b96] hover:cursor-pointer'>Show tags</p>
+                <p onClick={toggleTags} className='bg-[#3b71fc] text-white text-sm px-1 sm:px-2 md:font-[600] md:px-3 py-1 rounded-lg hover:bg-[#445b96] hover:cursor-pointer'>Show tags</p>
             )
         }
 
